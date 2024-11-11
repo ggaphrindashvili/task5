926a2cee538fdbae8f9eb004e1873e38eebdfaae
Gocha Gaprindashvili <ggaprindashvili@credo.ge>
2024-11-11 17:30:53 +0400

    Task_5_Gocha

--- src/test/java/convertToCelsius.java
+++ src/test/java/convertToCelsius.java
@@ -1,5 +1,9 @@
 public class convertToCelsius {
-    public static double convertToCelsius(double fahrenheit){
-        double celsius = fahrenheit - 32;
+    public static double convertToCelsius(double fahrenheit) {
+        return (5.0 / 9.0) * (fahrenheit - 32);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convertToCelsius(98.6));
     }
 }

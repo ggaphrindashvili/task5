9a0bdee45a8ea19d9c47aa9834da08c3bbecbe33
Gocha Gaprindashvili <ggaprindashvili@credo.ge>
2024-11-11 16:28:07 +0400

    Task_3_Gocha

---
+++ src/test/java/calculateArea.java
@@ -0,0 +1,9 @@
+public class calculateArea {
+    public static double calculateArea(double length, double width) {
+        return length * width;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateArea(5.5, 4.0));
+    }
+}

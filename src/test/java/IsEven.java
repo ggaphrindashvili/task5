dbfe6a436f710126df5ff1632046d00e1405f8ae
Gocha Gaprindashvili <ggaprindashvili@credo.ge>
2024-11-11 15:38:12 +0400

    Task_1_Gocha

---
+++ src/test/java/IsEven.java
@@ -0,0 +1,8 @@
+public class IsEven {
+    public static boolean isEven(int number) {return number % 2 == 0; }
+
+    public static void main(String[] args) {
+        System.out.println(isEven(4));
+        System.out.println(isEven(7));
+    }
+}
